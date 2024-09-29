196e373eb8140faddf7fd6f8f6e29a8acc16a417
Nmstr <publicNamester@gmail.com>
2024-09-29 11:12:32 +0200

    added basic timeline

--- LightDrive/main.py
+++ LightDrive/main.py
@@ -1,7 +1,58 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QGraphicsRectItem, \
+    QGraphicsEllipseItem, QPushButton, QGraphicsItem
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import QFile
+from PySide6.QtCore import QFile, Qt
+from PySide6.QtGui import QBrush
 import sys
+
+class Keyframe(QGraphicsEllipseItem):
+    def __init__(self, x, y, diameter):
+        super().__init__(x - diameter / 2, y - diameter / 2, diameter, diameter)
+        self.setBrush(Qt.blue)
+        self.setFlag(QGraphicsItem.ItemIsMovable)
+        self.setFlag(QGraphicsItem.ItemIsSelectable)
+
+class Timeline(QGraphicsView):
+    def __init__(self):
+        super().__init__()
+        self.scene = QGraphicsScene(self)
+        self.setScene(self.scene)
+
+        self.timelines = []
+        self.create_timeline()
+
+    def create_timeline(self):
+        y_position = len(self.timelines) * 60
+        timeline_rect = QGraphicsRectItem(0, y_position, 800, 50)
+        timeline_rect.setBrush(Qt.red)
+        timeline_rect.setOpacity(0.25)
+        self.scene.addItem(timeline_rect)
+        self.timelines.append(timeline_rect)
+        self.adjust_all_timeline_sizes(self.width(), 60)
+
+    def add_keyframe(self, position):
+        for timeline in self.timelines:
+            if timeline.rect().contains(position.x(), position.y()):
+                timeline_y_center = timeline.rect().center().y()
+                keyframe = Keyframe(position.x(), timeline_y_center, 10)
+                self.scene.addItem(keyframe)
+                break
+
+    def adjust_all_timeline_sizes(self, width, height):
+        for timeline in self.timelines:
+            timeline.setRect(0, timeline.rect().y(), width, height)
+
+    def change_timeline_color(self, index, color):
+        if 0 <= index < len(self.timelines):
+            timeline = self.timelines[index]
+            timeline.setBrush(QBrush(color))
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.RightButton:
+            # Mapping the clicked position from view coordinates to scene coordinates
+            position = self.mapToScene(event.pos())
+            self.add_keyframe(position)
+        super().mousePressEvent(event)
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
@@ -20,6 +71,30 @@ class MainWindow(QMainWindow):
         ui_file.close()
         self.setGeometry(self.ui.geometry())
         self.showMaximized()
+
+        self.timeline = Timeline()
+
+        # Add a button to add a new timeline
+        self.add_timeline_button = QPushButton("Add Timeline")
+        self.add_timeline_button.clicked.connect(self.add_timeline)
+
+        # Add timeline and button to layout
+        layout = self.ui.timeline.layout()
+        layout.addWidget(self.timeline)
+        layout.addWidget(self.add_timeline_button)
+
+        # Make the timeline the correct size initially
+        self.timeline.adjust_all_timeline_sizes(self.width(), 60)
+
+    def add_timeline(self):
+        self.timeline.create_timeline()
+
+    def resizeEvent(self, event):
+        try:
+            self.timeline.adjust_all_timeline_sizes(self.width(), 60)
+        except AttributeError:
+            pass
+        return super().resizeEvent(event)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
