60dc47c82344b25525e850215e6d033df3e4e574
Nmstr <publicNamester@gmail.com>
2024-10-28 17:02:01 +0100

    added basic workspace editor and console

---
+++ LightDrive/Workspace/Widgets/value_slider.py
@@ -0,0 +1,60 @@
+from PySide6.QtWidgets import QWidget, QSlider, QVBoxLayout, QSizePolicy, QSpinBox, QLabel
+from PySide6.QtGui import QPainter, QPixmap
+from PySide6.QtCore import Qt
+
+class JumpSlider(QSlider):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def mousePressEvent(self, event):
+        inverted_pos = (event.pos().y() * -1) + self.height()
+        reduced_position = inverted_pos / self.height()
+        self.setValue(reduced_position * self.maximum())
+        super().mousePressEvent(event)
+
+class ResetButton(QWidget):
+    def __init__(self, parent=None):
+        self.parent = parent
+        super().__init__(parent)
+        self.setFixedSize(50, 50)
+        self.icon = QPixmap("Assets/Icons/reset_button.svg")
+
+    def mousePressEvent(self, event):
+        self.parent.slider.setValue(0)
+        super().mousePressEvent(event)
+
+    def paintEvent(self, event) -> None:  # noqa: N802
+        """
+        Paint the button
+        :return: None
+        """
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.drawPixmap(self.rect(), self.icon)
+        super().paintEvent(event)
+
+class ValueSlider(QWidget):
+    def __init__(self, parent=None, index: int = 0):
+        super().__init__(parent)
+        layout = QVBoxLayout()
+
+        reset_button = ResetButton(self)
+        layout.addWidget(reset_button)
+
+        self.number_display = QSpinBox(self)
+        self.number_display.setRange(0, 255)
+        self.number_display.valueChanged.connect(lambda: self.slider.setValue(self.number_display.value()))
+        layout.addWidget(self.number_display)
+
+        self.slider = JumpSlider(self)
+        self.slider.setRange(0, 255)
+        self.slider.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        self.slider.valueChanged.connect(self.number_display.setValue)
+        layout.addWidget(self.slider)
+
+        label = QLabel(self)
+        label.setText(str(index + 1))
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(label)
+
+        self.setLayout(layout)
---
+++ LightDrive/workspace.py
@@ -0,0 +1,45 @@
+from Workspace.Widgets.value_slider import ValueSlider
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile
+import sys
+
+class Workspace(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setObjectName("Workspace")
+        self.setWindowTitle("LightDrive - Workspace")
+
+        # Load the stylesheet
+        with open('style.qss', 'r') as f:
+            app.setStyleSheet(f.read())
+
+        # Load the UI file
+        loader = QUiLoader()
+        ui_file = QFile("Workspace/workspace.ui")
+        self.ui = loader.load(ui_file, self)
+        ui_file.close()
+        self.setGeometry(self.ui.geometry())
+        self.showMaximized()
+
+        self.ui.fixture_btn.clicked.connect(lambda: self.show_page(0))
+        self.ui.console_btn.clicked.connect(lambda: self.show_page(1))
+
+        console_layout = self.ui.console_scroll_content.layout()
+        for i in range(255):
+            value_slider = ValueSlider(self, i)
+            console_layout.insertWidget(console_layout.count() - 1, value_slider)
+
+    def show_page(self, page_index: int) -> None:
+        """
+        Changes the current page in the content_page QStacked widget
+        :param page_index: The index of the page to switch to
+        :return: None
+        """
+        self.ui.content_page.setCurrentIndex(page_index)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Workspace()
+    window.show()
+    sys.exit(app.exec())
