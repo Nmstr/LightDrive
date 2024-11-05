06745d11927c9d3c38a05f035281d2eb7df1136e
Nmstr <publicNamester@gmail.com>
2024-11-05 21:19:27 +0100

    fixtures now display

--- LightDrive/Workspace/Dialogs/add_fixture_dialog.py
+++ LightDrive/Workspace/Dialogs/add_fixture_dialog.py
@@ -1,6 +1,8 @@
-from PySide6.QtWidgets import QDialog, QVBoxLayout
+from PySide6.QtWidgets import QDialog, QVBoxLayout, QTreeWidgetItem
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import QFile
+from PySide6.QtCore import QFile, Qt
+import json
+import os
 
 class AddFixtureDialog(QDialog):
     def __init__(self) -> None:
@@ -23,6 +25,31 @@ class AddFixtureDialog(QDialog):
         layout = QVBoxLayout()
         layout.addWidget(self.ui)
         self.setLayout(layout)
+
+        self.load_fixtures()
+
+    def load_fixtures(self) -> None:
+        """
+        Loads the fixtures and shows them in the fixture_selection_tree
+        :return: None
+        """
+        fixture_dir = os.getenv('XDG_CONFIG_HOME', default=os.path.expanduser('~/.config')) + '/LightDrive/fixtures/'
+        for fixture in os.listdir(fixture_dir):
+            with open(os.path.join(fixture_dir, fixture), 'r') as f:
+                fixture_data = json.load(f)
+
+            # Check if manufacturer entry already exists, and if not create it
+            manufacturer_item = self.ui.fixture_selection_tree.findItems(fixture_data["manufacturer"], Qt.MatchExactly)
+            if manufacturer_item:
+                manufacturer_item = manufacturer_item[0]  # Replace the one item list with just the item
+            else:
+                manufacturer_item = QTreeWidgetItem()
+                manufacturer_item.setText(0, fixture_data["manufacturer"])
+                self.ui.fixture_selection_tree.addTopLevelItem(manufacturer_item)
+
+            # Add the item
+            fixture_item = QTreeWidgetItem(manufacturer_item)
+            fixture_item.setText(0, fixture_data["name"])
 
     def accept(self):
         super().accept()
