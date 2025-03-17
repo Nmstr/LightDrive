4b027816fbd1ad21c5bf3c78881b65dfe149d65d
Nmstr <publicNamester@gmail.com>
2025-03-17 11:20:31 +0100

    directories now store children on save

--- LightDrive/Workspace/Widgets/snippet_tree_widget.py
+++ LightDrive/Workspace/Widgets/snippet_tree_widget.py
@@ -4,11 +4,14 @@ from PySide6.QtCore import Qt
 class SnippetTreeWidget(QTreeWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.window = parent
         self.setDragDropMode(QTreeWidget.DragDropMode.InternalMove)
 
     def dropEvent(self, event):  # noqa: N802
         target_item = self.itemAt(event.pos())
         if target_item and target_item.data(0, Qt.UserRole) == "directory":
             super().dropEvent(event)
+            dragged_item = event.source().selectedItems()[0]
+            self.window.snippet_manager.directory_manager.dir_add_snippet(target_item.uuid, dragged_item.uuid)
         else:
             event.ignore()
