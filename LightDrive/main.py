136ee41391957acbde78ba95691f8737f23d73c2
Nmstr <publicNamester@gmail.com>
2025-11-13 21:24:09 +0100

    moved main code to class

--- LightDrive/main.py
+++ LightDrive/main.py
@@ -7,30 +7,32 @@ from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtCore import QDir
 import sys
 
-def main() -> None:
-    workspace = Workspace()
+class LightDrive:
+    def __init__(self):
+        self.workspace = Workspace()
 
-    app = QGuiApplication(sys.argv)
-    engine = QQmlApplicationEngine()
+        app = QGuiApplication(sys.argv)
+        engine = QQmlApplicationEngine()
 
-    # Handlers
-    universe_handler = UniverseHandler(workspace)
-    engine.rootContext().setContextProperty("universeHandler", universe_handler)
+        # Handlers
+        self.universe_handler = UniverseHandler(self)
+        engine.rootContext().setContextProperty("universeHandler", self.universe_handler)
 
-    # Models
-    fixtures_model = build_fixtures_model()
-    engine.rootContext().setContextProperty("fixturesModel", fixtures_model)
-    snippet_model = build_snippet_model()
-    engine.rootContext().setContextProperty("snippetModel", snippet_model)
-    universe_list_model = build_universe_list_model()
-    engine.rootContext().setContextProperty("universeListModel", universe_list_model)
+        # Models
+        fixtures_model = build_fixtures_model()
+        engine.rootContext().setContextProperty("fixturesModel", fixtures_model)
+        snippet_model = build_snippet_model()
+        engine.rootContext().setContextProperty("snippetModel", snippet_model)
+        universe_list_model = build_universe_list_model()
+        engine.rootContext().setContextProperty("universeListModel", universe_list_model)
 
-    engine.addImportPath(QDir.currentPath() + "/qml")
-    engine.load("qml/main.qml")
+        engine.addImportPath(QDir.currentPath() + "/qml")
+        engine.load("qml/main.qml")
 
-    if not engine.rootObjects():
-        sys.exit(-1)
-    sys.exit(app.exec())
+        if not engine.rootObjects():
+            sys.exit(-1)
+        sys.exit(app.exec())
 
 if __name__ == "__main__":
-    main()
+    LightDrive()
+
--- LightDrive/universe_handler.py
+++ LightDrive/universe_handler.py
@@ -2,10 +2,10 @@ from data_structures import Universe
 from PySide6.QtCore import QObject, Slot
 
 class UniverseHandler(QObject):
-    def __init__(self, workspace):
+    def __init__(self, root):
         super().__init__()
-        self.workspace = workspace
+        self.root = root
 
     @Slot()
     def add_universe(self):
-        self.workspace.universes.append(Universe("Universe 1"))
+        self.root.workspace.universes.append(Universe("Universe 1"))
