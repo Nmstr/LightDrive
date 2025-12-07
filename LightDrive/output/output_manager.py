9b9695d47c1fde97e255f676dad1e09f33fa8f42
Nmstr <publicNamester@gmail.com>
2025-12-07 14:18:52 +0100

    output universes can now be build

--- LightDrive/output/output_manager.py
+++ LightDrive/output/output_manager.py
@@ -1,17 +1,27 @@
-from data_structures import Universe
-from output_snippets.generic_output_snippet import GenericOutputSnippet
-from output_universe import OutputUniverse
+from output.output_snippets.generic_output_snippet import GenericOutputSnippet
+from output.output_universe import OutputUniverse
 
 class OutputManager:
     def __init__(self, root):
         self.root = root
         self.universes: list[OutputUniverse] = []
 
-    def build_output_universes(self, universes: list[Universe]) -> None:
+    def build_output_universes(self) -> None:
         """
         Create the output universes as required.
         """
-        pass
+        for universe in self.root.workspace.universes:  # Add all universes
+            if universe.uuid in [universe.uuid for universe in self.universes]:
+                continue  # Universe already exists (here we don't force rebuild to preserve running snippets)
+
+            # Add the universe
+            output_universe = OutputUniverse(universe)
+            self.universes.append(output_universe)
+
+        # Remove universes that don't exist anymore
+        for universe in self.universes:
+            if universe.uuid not in [universe.uuid for universe in self.root.workspace.universes]:
+                self.universes.remove(universe)
 
     def add_snippet(self, universe_uuid: str, priority: int, snippet: GenericOutputSnippet) -> None:
         pass
--- LightDrive/output/output_universe.py
+++ LightDrive/output/output_universe.py
@@ -1,12 +1,11 @@
 from data_structures import Universe
-from output_snippets.generic_output_snippet import GenericOutputSnippet
-from output_backends.generic_output_backend import GenericOutputBackend
+from output.output_snippets.generic_output_snippet import GenericOutputSnippet
+from output.output_backends.generic_output_backend import GenericOutputBackend
 import queue
 
 class OutputUniverse:
-    def __init__(self, root, universe_data: Universe):
-        self.root = root
-        self.universe_data = universe_data
+    def __init__(self, universe_data: Universe):
+        self._universe_data = universe_data
         self.snippet_queue = queue.PriorityQueue()
         self.pending_removal_snippets: list[GenericOutputSnippet] = []
         self.output_backends: list[GenericOutputBackend] = []
@@ -25,4 +24,4 @@ class OutputUniverse:
 
     @property
     def uuid(self) -> str:
-        return self.universe_data.uuid
+        return self._universe_data.uuid
