d984df772728309c2f17c7e6051dcab16d81eea1
Nmstr <publicNamester@gmail.com>
2025-12-07 22:26:13 +0100

    implemented scene output snippet

--- LightDrive/output/output_snippets/generic_output_snippet.py
+++ LightDrive/output/output_snippets/generic_output_snippet.py
@@ -1,6 +1,17 @@
+from data_structures import Fixture
 from abc import ABC, abstractmethod
 
 class GenericOutputSnippet(ABC):
     @abstractmethod
-    def get_values(self):
+    def __init__(self, root):
+        self._root = root
+
+    @abstractmethod
+    def get_values(self) -> dict[int, int]:  # { channel_number: value }
         pass
+
+    def _get_fixture(self, fixture_uuid: str) -> Fixture | None:
+        for fixture in self._root.workspace.fixtures:
+            if fixture.uuid == fixture_uuid:
+                return fixture
+        return None
---
+++ LightDrive/output/output_snippets/scene_output_snippet.py
@@ -0,0 +1,18 @@
+from data_structures import SceneSnippet
+from output.output_snippets.generic_output_snippet import GenericOutputSnippet
+
+class SceneOutputSnippet(GenericOutputSnippet):
+    def __init__(self, root, scene_data: SceneSnippet) -> None:
+        self.scene_data = scene_data
+        super().__init__(root)
+
+    def get_values(self) -> dict[int, int]:
+        values = dict.fromkeys(range(256), 0)
+
+        for fixture_uuid, channels in self.scene_data.channel_values.items():
+            fixture = self._get_fixture(fixture_uuid)
+            for i, channel in enumerate(channels):
+                if channel.active:
+                    values[i + fixture.address] = channel.value
+
+        return values
